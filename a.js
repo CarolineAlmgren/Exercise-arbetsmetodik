81ba83ad616808d351400200c96f0ebf470c53f7
Carolinealmgren <carroalmgren89@gmail.com>
2024-01-16 20:59:35 +0100

    Changed variable names and made some other small fixes in the code to make it more clean

--- a.js
+++ a.js
@@ -5,32 +5,32 @@ import { faker } from 'https://cdn.skypack.dev/@faker-js/faker';
 //fixa så att det blir 108 000 kr istf 108000 kr
 
 class Car {
-    constructor(name,type,color,fuel,year,img,price){
-        this.name = name
-        this.type = type
-        this.color = color
-        this.fuel = fuel
-        this.year = year
-        this.img = "https://axmjqhyyjpat.objectstorage.eu-amsterdam-1.oci.customer-oci.com/n/axmjqhyyjpat/b/randomimages/o/cars%2F" + img +  ".png"
-        this.price = price
+  constructor(name, type, color, fuel, year, img, price) {
+    this.name = name;
+    this.type = type;
+    this.color = color;
+    this.fuel = fuel;
+    this.year = year;
+    this.img = "https://axmjqhyyjpat.objectstorage.eu-amsterdam-1.oci.customer-oci.com/n/axmjqhyyjpat/b/randomimages/o/cars%2F" + img +  ".png";
+    this.price = price;
    }
 }
 
-var cars = [];
+const cars = [];
 // Generera 10 bilar
-for (var carIndex = 0; carIndex < 10; carIndex++) {
-    var name = faker.vehicle.vehicle();
-    var type = faker.vehicle.type();
-    var color = faker.vehicle.color();
-    var fuel = faker.vehicle.fuel();
-    var year = faker.number.int({ min: 1970, max: 2023 });
-    var img = faker.number.int({ min: 1, max: 100 });
-    var price = faker.number.int({ min: 30, max: 200 }) * 1000;
-    cars.push(new Car(name, type, color, fuel, year, img, price));
+for (let carIndex = 0; carIndex < 10; carIndex++) {
+  const name = faker.vehicle.vehicle();
+  const type = faker.vehicle.type();
+  const color = faker.vehicle.color();
+  const fuel = faker.vehicle.fuel();
+  const year = faker.number.int({ min: 1970, max: 2023 });
+  const img = faker.number.int({ min: 1, max: 100 });
+  const price = faker.number.int({ min: 30, max: 200 }) * 1000;
+  cars.push(new Car(name, type, color, fuel, year, img, price));
 }
 
 function generateCarCards(cars)  {
-   return `<div class="productcard">
+  return `<div class="productcard">
     <img src="${cars.img}">
     <div class="information">
         <div class="name">${cars.name}</div>
