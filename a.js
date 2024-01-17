c2ce91dddae50dfcbd71fadfb19fcbc42b2153d1
Carolinealmgren <carroalmgren89@gmail.com>
2024-01-17 11:28:00 +0100

    Removed unnecessary code that isn't being used

--- a.js
+++ a.js
@@ -5,37 +5,30 @@ import { faker } from 'https://cdn.skypack.dev/@faker-js/faker';
 //fixa så att det blir 108 000 kr istf 108000 kr
 
 class Car {
-  constructor(name, type, color, fuel, year, img, price) {
+  constructor(name, year, img, price) {
     this.name = name;
-    this.type = type;
-    this.color = color;
-    this.fuel = fuel;
     this.year = year;
-    this.img = "https://axmjqhyyjpat.objectstorage.eu-amsterdam-1.oci.customer-oci.com/n/axmjqhyyjpat/b/randomimages/o/cars%2F" + img +  ".png";
+    this.img = "https://axmjqhyyjpat.objectstorage.eu-amsterdam-1.oci.customer-oci.com/n/axmjqhyyjpat/b/randomimages/o/cars%2F" + img + ".png";
     this.price = price;
-   }
+  }
 }
 
 const cars = [];
-// Generera 10 bilar
 for (let carIndex = 0; carIndex < 10; carIndex++) {
   const name = faker.vehicle.vehicle();
-  const type = faker.vehicle.type();
-  const color = faker.vehicle.color();
-  const fuel = faker.vehicle.fuel();
   const year = faker.number.int({ min: 1970, max: 2023 });
   const img = faker.number.int({ min: 1, max: 100 });
   const price = faker.number.int({ min: 30, max: 200 }) * 1000;
-  cars.push(new Car(name, type, color, fuel, year, img, price));
+  cars.push(new Car(name, year, img, price));
 }
 
-function generateCarCards(cars)  {
+function generateCarCards(car) {
   return `<div class="productcard">
-    <img src="${cars.img}">
+    <img src="${car.img}">
     <div class="information">
-        <div class="name">${cars.name}</div>
-        <div class="description">${cars.year}</div>
-        <a class="ctabutton" href="#">${cars.price} kr</a>
+        <div class="name">${car.name}</div>
+        <div class="description">${car.year}</div>
+        <a class="ctabutton" href="#">${car.price} kr</a>
         </div>
     </div>`;
 }
